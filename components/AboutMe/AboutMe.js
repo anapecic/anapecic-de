4f55d1d8993c251b3efa74b05a3352550aac34ba
Ana Pecic <ana.pecic97@gmail.com>
2024-08-23 14:16:28 +0200

    init

--- components/AboutMe/AboutMe.js
+++ components/AboutMe/AboutMe.js
@@ -30,21 +30,13 @@ const AboutWrapper = styled.section`
   background: #314028;
   color: #f4f1e8;
   padding: 8rem 4rem;
-  border-radius: 30% 70% 72% 28% / 37% 34% 66% 63%;
   position: relative;
 `;
 
 const StyledBackground = styled.div`
-  background: #9b4819;
-  padding: 7rem;
-  border-radius: 72% 28% 48% 52% / 41% 34% 66% 59%;
-  z-index: -1;
-  width: 400px;
-  height: 300px;
-  position: absolute;
-  left: -140px;
-  top: 30px;
-  transform: rotate(25deg);
+  width: 100px;
+  height: 100px;
+  background: #000000;
 `;
 
 const StyledText = styled.p`
@@ -56,37 +48,37 @@ const AboutLinks = styled.div``;
 
 export default function AboutMe() {
   return (
-    <AboutWrapper id="about-me">
-      <StyledBackground></StyledBackground>
-      <AboutLinks>
-        <Image
-          src="/Ana und Patchy cute.jpg"
-          width={300}
-          height={350}
-          alt="Ana und Hund"
-          style={{
-            borderRadius: "30% 70% 72% 28% / 37% 34% 66% 63%",
-            objectFit: "cover",
-          }}
-        />
-      </AboutLinks>
-      <AboutRechts>
-        <AboutHeadline className="about-headline">
-          <AboutTitle>About Me</AboutTitle>
-          {/* <Circle position="left: -40px; top: -45px;" scale="scale: 2.5;" /> */}
-        </AboutHeadline>
-        <StyledText>
-          Diving headfirst into the world of frontend web development at the
-          moment. Currently, I&apos;m sharpening my skills at neuefische&rsquo;s
-          web development bootcamp, and I&apos;m all about creating sleek,
-          responsive web interfaces.
-          <br />
-          <br />
-          With a solid background in digital marketing, large-scale social media
-          projects, and influencer management, I&apos;m super excited to bring
-          that experience into tech.
-        </StyledText>
-      </AboutRechts>
-    </AboutWrapper>
+    <>
+      <AboutWrapper id="about-me">
+        <AboutLinks>
+          <Image
+            src="/Ana und Patchy cute.jpg"
+            width={300}
+            height={350}
+            alt="Ana und Hund"
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </AboutLinks>
+        <AboutRechts>
+          <AboutHeadline className="about-headline">
+            <AboutTitle>About Me</AboutTitle>
+            {/* <Circle position="left: -40px; top: -45px;" scale="scale: 2.5;" /> */}
+          </AboutHeadline>
+          <StyledText>
+            Diving headfirst into the world of frontend web development at the
+            moment. Currently, I&apos;m sharpening my skills at
+            neuefische&rsquo;s web development bootcamp, and I&apos;m all about
+            creating sleek, responsive web interfaces.
+            <br />
+            <br />
+            With a solid background in digital marketing, large-scale social
+            media projects, and influencer management, I&apos;m super excited to
+            bring that experience into tech.
+          </StyledText>
+        </AboutRechts>
+      </AboutWrapper>
+    </>
   );
 }
